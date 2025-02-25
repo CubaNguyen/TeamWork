774d6d40f7a3535f4f30db6889c3a4cbd5820f33
DANGCHUONG0203 <dangvanchuong2004@gmail.com>
2025-02-25 21:36:32 +0700

    Da lam xong About

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import About from './components/About/Abouts';
+import OurCollection from './components/OurCollection/OurCollection';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <OurCollection/>
   </React.StrictMode>
 );
 
