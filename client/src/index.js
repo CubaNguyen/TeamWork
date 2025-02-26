bd6beb4b16b3252a696292558e56103f34b94994
vuthanhdat041 <dathanh04@gmail.com>
2025-02-26 14:08:51 +0700

    luu cac thay doi

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Gift from './components/Gift/gift';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    
+    <Gift/>
     <App />
   </React.StrictMode>
 );
