1baf2e8e6f488cf4e1187b4b956a585da8aa1301
Dang Van Chuong <dangvanchuong2004@gmail.com>
2025-04-15 10:11:19 +0700

    k

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import OrderManagement from "./Admin/OrderManagement/OrderManagement";
 import OrderHistory from "./Admin/OrderManagement/OrderHistory";
 
 function App() {
   return (
-    <div>
-      
-
-      <Routes>
-        <Route path="/order-management" element={<OrderManagement />} />
-        <Route path="/order-history" element={<OrderHistory />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/order-management" />} />
+      <Route path="/order-management" element={<OrderManagement />} />
+      <Route path="/order-history" element={<OrderHistory />} />
+    </Routes>
   );
 }
 
